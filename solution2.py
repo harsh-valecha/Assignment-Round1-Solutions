e4163228d4640a97f09544e3ccc0f9970d58ad9d
Harsh Valecha <119730682+harsh-valecha@users.noreply.github.com>
2022-12-10 14:05:05 +0530

    Update solution2.py

--- solution2.py
+++ solution2.py
@@ -7,52 +7,51 @@ phone = []
 social = []
 
 
-url= input("Enter URL:")
-
-#sending get request
-r = requests.get(url)
-if r.status_code == 200:
-    # fetching page source code
-    with open("index.html","wb") as f:
-        f.write(r.content)
-
-
+# reference lists
 social_media = ["facebook","linkedin","github","youtube","twitter","instagram","discord"]
 contact = ["tel:","phone:","mobile:"]
 
-# scraping information from html file
-with open("index.html","rb") as f:
-    soup = BeautifulSoup(f,'html.parser')
-    for link in soup.find_all('a'):
-        if link.has_attr('href'):
-            href = link.get('href')
-            # social links
-            if href.startswith('https://'):
-                social.extend([href for i in social_media if (i in href) and (href not in social)])
 
-            if ('mailto' in href):
-                mail = href.split('?')[0][7:]
-                email.append(mail)
-                
-            # contact
-            m1= href.lower()
-            phone.extend([href.split(':')[1] for i in contact if i in m1])
+url= input("Enter URL:")
+
+#sending get request
+try :
+    r = requests.get(url)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.content,'html.parser')
+        for link in soup.find_all('a'):
+            if link.has_attr('href'):
+                href = link.get('href')
+                # social links
+                if href.startswith('https://'):
+                    social.extend([href for i in social_media if (i in href) and (href not in social)])
+
+                # emails
+                if ('mailto' in href):
+                    mail = href.split('?')[0][7:]
+                    email.append(mail)
                     
+                # contact
+                m1= href.lower()
+                phone.extend([href.split(':')[1] for i in contact if i in m1])
+    # Output
+    print("Social Links - ")
+    for i in social:
+        print(i )
+    print()
+    print("Email/s - ")
+    for i in email:
+        print(i, end = '\n')
+    print()
+    print('Contact:')
+    for i in phone:
+        print(i)
+        
+    else:
+        print(f"Response Status Code :{r.status_code}")
+except Exception as err:
+    print(err)
 
-
-# Output
-print("Social Links - ")
-for i in social:
-    print(i )
-print()
-print("Email:")
-for i in email:
-    print(i, end = '\n')
-print()
-print('Contact:')
-for i in phone:
-    print(i)
-    
 
 
 
